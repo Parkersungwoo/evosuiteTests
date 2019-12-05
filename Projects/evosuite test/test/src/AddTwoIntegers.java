16462ff6c4865ae433047c9d47d251dad2286d12
Joey Ballentine <joeyjballentine@gmail.com>
2019-12-05 12:14:35 -0600

    Added more to the custom class that I ran EvoSuite on

--- Projects/evosuite test/test/src/AddTwoIntegers.java
+++ Projects/evosuite test/test/src/AddTwoIntegers.java
@@ -13,4 +13,27 @@ public class AddTwoIntegers {
         public static int add(int int1, int int2) {
             return int1 + int2;
         }
+
+    public static int subtract(int int1, int int2) {
+        return int1 - int2;
+    }
+
+    public static int multiply(int int1, int int2) {
+        return int1 * int2;
+    }
+
+    public static String substring(String s, int length) {
+            return s.substring(0, length);
+    }
+
+    public static String wacky(String s) {
+        String output = s.replace("a", "e");
+        output = output.replace("b", "c");
+        output = output.replace("f", "p");
+        output = output.replace("t", "v");
+        output = output.toUpperCase();
+        return output;
+    }
+
+
     }
